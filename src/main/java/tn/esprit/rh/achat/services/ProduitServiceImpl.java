8678db9f63dfa153976256677c357e8c5016f70d
faroukgasara <farouk.gassara@esprit.tn>
2022-10-19 02:21:47 +0100

    first

--- src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/ProduitServiceImpl.java
@@ -1,8 +1,9 @@
 package tn.esprit.rh.achat.services;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
 import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
